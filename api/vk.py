384c01555974af8c72864c9a2630bbe0d54817f4
Fumycat <fumycat@gmail.com>
2016-12-15 14:45:22 +0700

    messages_send

--- api/vk.py
+++ api/vk.py
@@ -20,11 +20,8 @@ def users_get(user_ids=1, fields=None, name_case='nom'):
         user_ids = ','.join([str(f) for f in user_ids])
     if isinstance(fields, list):
         fields = ','.join([str(f) for f in fields])
-    parameters = {'user_ids': user_ids,
-                  'fields': fields,
-                  'name_case': name_case,
-                  'access_token': os.environ['VK_TOKEN'],
-                  'v': os.environ['API_VERSION']}
+    parameters = dict(user_ids=user_ids, fields=fields, name_case=name_case, access_token=os.environ['VK_TOKEN'],
+                      v=os.environ['API_VERSION'])
     request = requests.get(API + 'users.get', params=parameters)
     response = request.json()
     return result_parser(response, UsersGetException)
@@ -33,9 +30,8 @@ def users_get(user_ids=1, fields=None, name_case='nom'):
 def wall_get(target=0, count=None, offset=None, filter=None, fields=None):
     if isinstance(fields, list):
         fields = ','.join([str(f) for f in fields])
-    parameters = {'fields': fields, 'offset': offset, 'count': count, 'filter': filter,
-                  'access_token': os.environ['VK_TOKEN'], 'v': os.environ['API_VERSION'],
-                  'extended': 0 if not fields else 1}
+    parameters = dict(fields=fields, offset=offset, count=count, filter=filter, access_token=os.environ['VK_TOKEN'],
+                      v=os.environ['API_VERSION'], extended=0 if not fields else 1)
     if isinstance(target, str):
         parameters['domain'] = target
     else:
@@ -48,9 +44,21 @@ def wall_get(target=0, count=None, offset=None, filter=None, fields=None):
 
 
 def likes_add(owner_id=None, item_id=0, type='post', access_key=None):
-    parameters = {'access_token': os.environ['VK_TOKEN'], 'v': os.environ['API_VERSION'],
-                  'owner_id': owner_id, 'item_id': item_id, 'type': type, 'access_key': access_key}
+    parameters = dict(access_token=os.environ['VK_TOKEN'], v=os.environ['API_VERSION'], owner_id=owner_id,
+                      item_id=item_id, type=type, access_key=access_key)
     request = requests.get(API + 'likes.add', params=parameters)
+    if 'response' in request.json():
+        return True
+    else:
+        return request.json()['error']
+
+
+def messages_send(user_id=None, peer_id=None, domain=None, chat_id=None,
+                  message=None, attachment=None, forward_messages=None, sticker_id=None):
+    parameters = dict(access_token=os.environ['VK_TOKEN'], v=os.environ['API_VERSION'], user_id=user_id,
+                      peer_id=peer_id, domain=domain, chat_id=chat_id, message=message, attachment=attachment,
+                      forward_messages=forward_messages, sticker_id=sticker_id)
+    request = requests.get(API + 'messages.send', params=parameters)
     if 'response' in request.json():
         return True
     else:
--- core.py
+++ core.py
@@ -2,12 +2,14 @@ from parse import parse
 from api.vk import users_get
 from api.vk import wall_get
 from api.vk import likes_add
+from api.vk import messages_send
 
 
 def main():
     #  print(users_get(1, ['online', 'sex']))
     #  print(wall_get('catcontrolcenter', 1))
-    print(likes_add(-135179098, 2))
+    #  print(likes_add(-135179098, 2))
+    print(messages_send(domain='fumycat', message='Test'))
     return
 
 main()
